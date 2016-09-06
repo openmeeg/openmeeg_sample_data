1fada03dfd16aa4355ab0949803852f70d90a9dc
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-09-06 08:42:35 +0200

    some py3k + pep8

--- compute_leadfields.py
+++ compute_leadfields.py
@@ -7,21 +7,24 @@
     - Internal Potential for dipolar source
     - Internal Potential for boundary injected current
 """
-print __doc__
+
+from __future__ import print_function
 
 import openmeeg as om
 from os import path as op
 
+print(__doc__)
+
 ###############################################################################
 # Load data
-geom_file                    = 'data/model/head_model.geom'
-cond_file                    = 'data/model/head_model.cond'
-dipoles_file                 = 'data/model/cortex_dipoles.txt'
-squids_file                  = 'data/model/meg_channels_locations.squids'
-eeg_electrodes_file          = 'data/model/eeg_channels_locations.txt'
-eit_electrodes_file          = 'data/model/eit_locations.txt'
-ecog_electrodes_file         = 'data/model/ecog_electrodes_locations.txt'
-internal_electrodes_file     = 'data/model/internal_electrodes_locations.txt'
+geom_file = 'data/model/head_model.geom'
+cond_file = 'data/model/head_model.cond'
+dipoles_file = 'data/model/cortex_dipoles.txt'
+squids_file = 'data/model/meg_channels_locations.squids'
+eeg_electrodes_file = 'data/model/eeg_channels_locations.txt'
+eit_electrodes_file = 'data/model/eit_locations.txt'
+ecog_electrodes_file = 'data/model/ecog_electrodes_locations.txt'
+internal_electrodes_file = 'data/model/internal_electrodes_locations.txt'
 
 geom = om.Geometry(geom_file, cond_file)
 
@@ -38,7 +41,7 @@ ecog_electrodes = om.Sensors(ecog_electrodes_file)
 int_electrodes = om.Matrix(internal_electrodes_file)
 
 ###############################################################################
-#create a dir for leadfields and tmp
+# create a dir for leadfields and tmp
 if not op.exists("tmp"):
     import os
     os.mkdir('tmp')
@@ -53,7 +56,7 @@ dipole_in_cortex = True
 
 if op.exists("tmp/hmi.mat"):
     hminv = om.SymMatrix("tmp/hmi.mat")
-    print "HM inverse loaded from ", "tmp/hmi.mat"
+    print("HM inverse loaded from ", "tmp/hmi.mat")
 else:
     hm = om.HeadMat(geom, gauss_order)
     hm.invert()
@@ -63,7 +66,7 @@ else:
 
 if op.exists("tmp/dsm.mat"):
     dsm = om.Matrix("tmp/dsm.mat")
-    print "DSM loaded from ", "tmp/dsm.mat"
+    print("DSM loaded from ", "tmp/dsm.mat")
 else:
     dsm = om.DipSourceMat(geom, dipoles, gauss_order, use_adaptive_integration, "Brain")
     dsm.save("tmp/dsm.mat")
@@ -85,26 +88,26 @@ eitsm = om.EITSourceMat(geom, eit_electrodes, gauss_order)
 iphm = om.Surf2VolMat(geom, int_electrodes)
 ipsm = om.DipSource2InternalPotMat(geom, dipoles, int_electrodes)
 
-eeg_leadfield  = om.GainEEG(hminv, dsm, h2em)
+eeg_leadfield = om.GainEEG(hminv, dsm, h2em)
 ecog_leadfield = om.GainEEG(hminv, dsm, h2ecogm)
-meg_leadfield  = om.GainMEG(hminv, dsm, h2mm, ds2mm)
-eit_leadfield  = om.GainEEG(hminv, eitsm, h2em)
-ip_leadfield   = om.GainInternalPot(hminv, dsm, iphm, ipsm)
-sip_leadfield  = om.GainStimInternalPot(hminv, eitsm,  iphm)
+meg_leadfield = om.GainMEG(hminv, dsm, h2mm, ds2mm)
+eit_leadfield = om.GainEEG(hminv, eitsm, h2em)
+ip_leadfield = om.GainInternalPot(hminv, dsm, iphm, ipsm)
+sip_leadfield = om.GainStimInternalPot(hminv, eitsm, iphm)
 # eeg_leadfield_adjoint = om.GainEEGadjoint(geom,dipoles,hm, h2em)
 
-print "hminv          : %d x %d" % (hminv.nlin(), hminv.ncol())
-print "dsm            : %d x %d" % (dsm.nlin(), dsm.ncol())
-print "h2em           : %d x %d" % (h2em.nlin(), h2em.ncol())
-print "h2ecogm        : %d x %d" % (h2ecogm.nlin(), h2ecogm.ncol())
-print "ds2mm          : %d x %d" % (ds2mm.nlin(), ds2mm.ncol())
-print "h2mm           : %d x %d" % (h2mm.nlin(), h2mm.ncol())
-print "eeg_leadfield  : %d x %d" % (eeg_leadfield.nlin(), eeg_leadfield.ncol())
-print "ecog_leadfield : %d x %d" % (ecog_leadfield.nlin(), ecog_leadfield.ncol())
-print "meg_leadfield  : %d x %d" % (meg_leadfield.nlin(), meg_leadfield.ncol())
-print "eit_leadfield  : %d x %d" % (eit_leadfield.nlin(), eit_leadfield.ncol())
-print "ip_leadfield   : %d x %d" % (ip_leadfield.nlin(), ip_leadfield.ncol())
-print "sip_leadfield  : %d x %d" % (sip_leadfield.nlin(), sip_leadfield.ncol())
+print("hminv          : %d x %d" % (hminv.nlin(), hminv.ncol()))
+print("dsm            : %d x %d" % (dsm.nlin(), dsm.ncol()))
+print("h2em           : %d x %d" % (h2em.nlin(), h2em.ncol()))
+print("h2ecogm        : %d x %d" % (h2ecogm.nlin(), h2ecogm.ncol()))
+print("ds2mm          : %d x %d" % (ds2mm.nlin(), ds2mm.ncol()))
+print("h2mm           : %d x %d" % (h2mm.nlin(), h2mm.ncol()))
+print("eeg_leadfield  : %d x %d" % (eeg_leadfield.nlin(), eeg_leadfield.ncol()))
+print("ecog_leadfield : %d x %d" % (ecog_leadfield.nlin(), ecog_leadfield.ncol()))
+print("meg_leadfield  : %d x %d" % (meg_leadfield.nlin(), meg_leadfield.ncol()))
+print("eit_leadfield  : %d x %d" % (eit_leadfield.nlin(), eit_leadfield.ncol()))
+print("ip_leadfield   : %d x %d" % (ip_leadfield.nlin(), ip_leadfield.ncol()))
+print("sip_leadfield  : %d x %d" % (sip_leadfield.nlin(), sip_leadfield.ncol()))
 
 eeg_leadfield.save('leadfields/eeg_leadfield.mat')
 # eeg_leadfield_adjoint.save('leadfields/eeg_leadfield_adjoint.mat')
